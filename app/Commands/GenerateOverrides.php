984130b6e64367f681aadf3af45ce5ecf965dce6
Nick Poulos <nickpoulos.ny@gmail.com>
2021-11-02 00:12:56 -0400

    Fix command signature, add some more output

--- app/Commands/GenerateOverrides.php
+++ app/Commands/GenerateOverrides.php
@@ -3,11 +3,9 @@
 namespace App\Commands;
 
 use App\EzRider\EzRider;
-use App\EzRider\Plugins\PluginLoader;
 use Wilderborn\Partyline\Facade as Partyline;
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
-use Illuminate\Contracts\Container\BindingResolutionException;
 
 class GenerateOverrides extends Command
 {
@@ -16,14 +14,14 @@ class GenerateOverrides extends Command
      *
      * @var string
      */
-    protected $signature = 'generate {config=ezrider.json}';
+    protected $signature = 'generate {config?}';
 
     /**
      * The description of the command.
      *
      * @var string
      */
-    protected $description = 'Generate overrides files for Docker Compose that automatically fetches secrets';
+    protected $description = 'Generate overrides files for Docker Compose that can automatically generate or fetch sensitive data';
 
     /**
      * Execute the console command.
@@ -33,11 +31,14 @@ class GenerateOverrides extends Command
     public function handle(EzRider $ezRider)
     {
         Partyline::bind($this);
+
         $configFile = $this->argument('config');
 
         try {
-            $ezRider->loadConfig($configFile);
+            $configFile = $ezRider->loadConfig($configFile);
+            $this->info('Ez-rider configuration loaded from: ' . $configFile);
             $ezRider->generateOverrideFiles();
+            $this->info('Done.');
         } catch (\Exception $e) {
             $this->error($e->getMessage());
             return 1;
